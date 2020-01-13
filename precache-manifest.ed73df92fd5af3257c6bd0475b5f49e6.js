1bd724cee603d2b66900303d09166373a921feb2
Alon Joshua <alonjoshua@gmail.com>
2020-01-13 18:48:34 +0200

    Updates

---
+++ precache-manifest.ed73df92fd5af3257c6bd0475b5f49e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bda2cae7b01ea273c1314852f513fd9d",
+    "url": "/num-gen/index.html"
+  },
+  {
+    "revision": "4cbf5ebd86b0ca3e88d2",
+    "url": "/num-gen/static/css/main.af84b316.chunk.css"
+  },
+  {
+    "revision": "33cb698ab07873033bc5",
+    "url": "/num-gen/static/js/2.961e2f09.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/num-gen/static/js/2.961e2f09.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4cbf5ebd86b0ca3e88d2",
+    "url": "/num-gen/static/js/main.6969f743.chunk.js"
+  },
+  {
+    "revision": "6b3635a41aac397c5623",
+    "url": "/num-gen/static/js/runtime-main.c8341f9a.js"
+  }
+]);
